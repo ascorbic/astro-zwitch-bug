a93b1167d98be26c95b93dd644e920f840f555fd
Matt Rothenberg <mattrothenberg@gmail.com>
2025-03-17 12:36:06 -0400

    add vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import markdoc from '@astrojs/markdoc';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [markdoc()],
+  adapter: vercel()
+});
